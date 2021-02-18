36617b61c3f43585dfcfe57e5745e8379bde86d8
Chav Sumalinog <chaviotics@gmail.com>
2021-02-18 13:48:57 +0800

    Finished Section 21.

---
+++ 21. Machine Learning + Data Science/ReallySmartBrain/brain.py
@@ -0,0 +1,15 @@
+from imageai import Prediction
+from imageai.Prediction import ImagePrediction
+import os
+
+execution_path = os.getcwd()
+# print(execution_path)
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsMobileNetV2() # decide what model we're going to use 
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "house.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
